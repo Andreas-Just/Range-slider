0cb80e59b9d80f43267f5df764ff6df686b0b575
Andreas_Just <andreas.wq@gmail.com>
2019-01-27 03:17:57 +0200

    The element of the left thumb is complete

--- script.js
+++ script.js
@@ -1,52 +1,77 @@
 'use strict';
 
-CreateSlider.prototype._onDocumentMousemove = function(event) {
-	let x = event.clientX - this._containerPosition.x - this._thumbElement.offsetWidth / 2;
-	let maxX = this._containerElement.clientWidth - this._thumbElement.offsetWidth;
-
-	if (x < 0) {
-		x = 0;
+CreateSlider.prototype._onThumbMouseDown = function(event) {
+	if (event.target === this._thumbElementLeft) {
+		document.addEventListener('mousemove', this._mouseMoveForSlider);
+		document.addEventListener('mouseup', this._mouseUpForSlider);
+	} else {
+		event.target.addEventListener('click', this._clickMoveOnSlider);
 	}
-
-	if (x > maxX) {
-		x = maxX;
-	}
-
-	this._thumbElement.style.left = x + 'px';
 };
+
+CreateSlider.prototype._onThumbClickMove = function(event) {
+	this._xThumbLeft = event.clientX - this._containerPosition.x  - this._thumbElementLeft.clientWidth / 2;
+
+	this._thumbElementLeft.style.left = this._xThumbLeft + 'px';
+	this._fillElement.style.width =  parseInt(this._thumbElementLeft.style.left) + 5 + 'px';
+};
+
+CreateSlider.prototype._onDocumentMousemove = function(event) {
+	let maxX = this._xThumbRight;
+
+	this._xThumbRight = this._thumbElementRightPosition.x - this._containerPosition.x - this._thumbElementRight.offsetWidth;
+	this._xThumbLeft = event.clientX - this._containerPosition.x - this._thumbElementLeft.clientWidth / 2;
+
+	if (this._xThumbLeft < 0) {
+		this._xThumbLeft = 0;
+	}
+
+	if (this._xThumbLeft > maxX) {
+		this._xThumbLeft = maxX;
+	}
+
+	this._thumbElementLeft.style.left = this._xThumbLeft + 'px';
+	this._fillElement.style.width =  parseInt(this._thumbElementLeft.style.left) + 5 + 'px';
+};
+
 CreateSlider.prototype._onDocumentMouseup = function() {
 	document.removeEventListener('mousemove', this._mouseMoveForSlider);
 	document.removeEventListener('mouseup', this._mouseUpForSlider);
 };
-CreateSlider.prototype._onThumbMouseDown = function() {
-	document.addEventListener('mousemove', this._mouseMoveForSlider);
-	document.addEventListener('mouseup', this._mouseUpForSlider);
-};
+
+
 
 function CreateSlider(sliderElement) {
-	// this === slider1
-	let x = 1;
-
 	sliderElement.innerHTML = `
     <div class="slider__value range">
 			<div class="range__value range__value--min">10</div>
 			<div class="range__value range__value--max">9990</div>
 		</div>
 		<div class="slider__container">
+			<div class="slider__fill"></div>
 			<div class="slider__thumb slider__thumb--left"></div>
 			<div class="slider__thumb slider__thumb--right"></div>
 		</div>
   `;
 
+	this._mouseDownForSlider = this._onThumbMouseDown.bind(this);
 	this._mouseMoveForSlider = this._onDocumentMousemove.bind(this);
 	this._mouseUpForSlider = this._onDocumentMouseup.bind(this);
-	this._mouseDownForSlider = this._onThumbMouseDown.bind(this);
+	this._clickMoveOnSlider = this._onThumbClickMove.bind(this);
 
-	this._thumbElement = sliderElement.querySelector('.slider__thumb--left');
+	this._thumbElementLeft = sliderElement.querySelector('.slider__thumb--left');
+	this._thumbElementRight = sliderElement.querySelector('.slider__thumb--right');
 	this._containerElement = sliderElement.querySelector('.slider__container');
+	this._fillElement = sliderElement.querySelector('.slider__fill');
 	this._containerPosition = this._containerElement.getBoundingClientRect();
+	this._thumbElementLeftPosition = this._thumbElementLeft.getBoundingClientRect();
+	this._thumbElementRightPosition = this._thumbElementRight.getBoundingClientRect();
 
-	this._thumbElement.addEventListener('mousedown', this._mouseDownForSlider);
+	this._xThumbLeft = this._thumbElementLeftPosition.x - this._thumbElementLeft.clientWidth / 2;
+	this._xThumbRight = this._thumbElementRightPosition.x - this._thumbElementRight.clientWidth / 2;
+	this._fillElement.style.width = this._thumbElementLeftPosition.x - this._containerPosition.x + 5 + 'px';
+
+	this._containerElement.addEventListener('mousedown', this._mouseDownForSlider);
 }
 
 const slider1 = new CreateSlider( document.querySelector('#slider1') );

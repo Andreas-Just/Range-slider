4ee682dd12604dbb338ede5c9ae56a50f9129c5a
Andreas_Just <andreas.wq@gmail.com>
2019-01-27 17:07:50 +0200

    The element of the right thumb is starting

--- script.js
+++ script.js
@@ -13,13 +13,11 @@ CreateSlider.prototype._onThumbClickMove = function(event) {
 	this._xThumbLeft = event.clientX - this._containerPosition.x  - this._thumbElementLeft.clientWidth / 2;
 
 	this._thumbElementLeft.style.left = this._xThumbLeft + 'px';
-	this._fillElement.style.width =  parseInt(this._thumbElementLeft.style.left) + 5 + 'px';
+	this._fillElementLeft.style.width =  parseInt(this._thumbElementLeft.style.left) + 5 + 'px';
 };
 
 CreateSlider.prototype._onDocumentMousemove = function(event) {
-	let maxX = this._xThumbRight;
-
-	this._xThumbRight = this._thumbElementRightPosition.x - this._containerPosition.x - this._thumbElementRight.offsetWidth;
+	let maxX = this._thumbElementRightPosition.x - this._containerPosition.x - this._thumbElementRight.offsetWidth;
 	this._xThumbLeft = event.clientX - this._containerPosition.x - this._thumbElementLeft.clientWidth / 2;
 
 	if (this._xThumbLeft < 0) {
@@ -31,7 +29,7 @@ CreateSlider.prototype._onDocumentMousemove = function(event) {
 	}
 
 	this._thumbElementLeft.style.left = this._xThumbLeft + 'px';
-	this._fillElement.style.width =  parseInt(this._thumbElementLeft.style.left) + 5 + 'px';
+	this._fillElementLeft.style.width =  parseInt(this._thumbElementLeft.style.left) + 5 + 'px';
 };
 
 CreateSlider.prototype._onDocumentMouseup = function() {
@@ -45,10 +43,11 @@ function CreateSlider(sliderElement) {
 	sliderElement.innerHTML = `
     <div class="slider__value range">
 			<div class="range__value range__value--min">10</div>
-			<div class="range__value range__value--max">9990</div>
+			<div class="range__value range__value--max">10000</div>
 		</div>
 		<div class="slider__container">
-			<div class="slider__fill"></div>
+			<div class="slider__fill slider__fill--left"></div>
+			<div class="slider__fill slider__fill--right"></div>
 			<div class="slider__thumb slider__thumb--left"></div>
 			<div class="slider__thumb slider__thumb--right"></div>
 		</div>
@@ -62,14 +61,16 @@ function CreateSlider(sliderElement) {
 	this._thumbElementLeft = sliderElement.querySelector('.slider__thumb--left');
 	this._thumbElementRight = sliderElement.querySelector('.slider__thumb--right');
 	this._containerElement = sliderElement.querySelector('.slider__container');
-	this._fillElement = sliderElement.querySelector('.slider__fill');
+	this._fillElementLeft = sliderElement.querySelector('.slider__fill--left');
+	this._fillElementRight = sliderElement.querySelector('.slider__fill--right');
 	this._containerPosition = this._containerElement.getBoundingClientRect();
 	this._thumbElementLeftPosition = this._thumbElementLeft.getBoundingClientRect();
 	this._thumbElementRightPosition = this._thumbElementRight.getBoundingClientRect();
 
-	this._xThumbLeft = this._thumbElementLeftPosition.x - this._thumbElementLeft.clientWidth / 2;
-	this._xThumbRight = this._thumbElementRightPosition.x - this._thumbElementRight.clientWidth / 2;
-	this._fillElement.style.width = this._thumbElementLeftPosition.x - this._containerPosition.x + 5 + 'px';
+	// this._xThumbLeft = this._thumbElementLeftPosition.x - this._thumbElementLeft.clientWidth / 2;
+	// this._xThumbRight = this._thumbElementRightPosition.x - this._thumbElementRight.clientWidth / 2;
+	this._fillElementLeft.style.width = this._thumbElementLeftPosition.x - this._containerPosition.x + 5 + 'px';
+	this._fillElementRight.style.width = this._containerPosition.x + this._containerElement.clientWidth - this._thumbElementRightPosition.x - 5 + 'px';
 
 	this._containerElement.addEventListener('mousedown', this._mouseDownForSlider);
 }
